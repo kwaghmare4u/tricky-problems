041496f45f630513547cb089edbb8e4222a6c0b4
kwaghmare4u <kwaghmare4u@gmail.com>
2023-10-09 12:50:20 +0530

    Print 1-100 without using numbers

---
+++ src/main/java/PrintNumbersOneToHundred.java
@@ -0,0 +1,16 @@
+package main.java;
+
+public class PrintNumbersOneToHundred {
+
+    public static void main(String[] args) {
+        // Output numbers from 1..100
+        // without using any numbers in your code
+
+        int num = 'A'/'A';
+        String sampleString = "..........";
+        for (int i = num; i <= sampleString.length() * sampleString.length(); i++) {
+            System.out.println("Value :" + i);
+
+        }
+    }
+}
